78d2ae7589700b58709661fd4abc9336e58f70e5
Julian Amado Caicaguare Caicaguan <caicaguarec@gmail.com>
2023-03-27 12:43:47 -0400

    cambios realizados con el merge y modulo proyecto

--- app/Models/Corporacione.php
+++ app/Models/Corporacione.php
@@ -12,15 +12,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property $rif
  * @property $imagen
  * @property $telefono
-<<<<<<< HEAD
- * @property $gabinete_id
- * @property $direcion_id
- * @property $responsable
- * @property $correo
- * @property $created_at
- * @property $updated_at
- *
-=======
  * @property $responsable
  * @property $correo
  * @property $gabinete_id
@@ -32,7 +23,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property Gabinete $gabinete
  * @property Proyecto[] $proyectos
  * @property Responsable[] $responsables
->>>>>>> proyecto
  * @package App
  * @mixin \Illuminate\Database\Eloquent\Builder
  */
@@ -44,17 +34,10 @@ class Corporacione extends Model
 		'rif' => 'required',
 		'imagen' => 'required',
 		'telefono' => 'required',
-<<<<<<< HEAD
-		'gabinete_id' => 'required',
-		'direcion_id' => 'required',
-		'responsable' => 'required',
-		'correo' => 'required',
-=======
 		'responsable' => 'required',
 		'correo' => 'required',
 		'gabinete_id' => 'required',
 		'direcion_id' => 'required',
->>>>>>> proyecto
     ];
 
     protected $perPage = 20;
@@ -64,11 +47,6 @@ class Corporacione extends Model
      *
      * @var array
      */
-<<<<<<< HEAD
-    protected $fillable = ['nombre','rif','imagen','telefono','gabinete_id','direcion_id','responsable','correo'];
-
-
-=======
     protected $fillable = ['nombre','rif','imagen','telefono','responsable','correo','gabinete_id','direcion_id'];
 
 
@@ -104,6 +82,5 @@ class Corporacione extends Model
         return $this->hasMany('App\Models\Responsable', 'corporacion_id', 'id');
     }
     
->>>>>>> proyecto
 
 }
